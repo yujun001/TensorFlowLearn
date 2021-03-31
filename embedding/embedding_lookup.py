ce545d47ff76b4dbcf3858043527ab617a355c00
549062405@qq.com <yujun001>
2021-03-31 13:55:28 +0800

    commit

---
+++ embedding/embedding_lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/3/31 下午1:48
+@Author  : YuJun
+@FileName: embedding_lookup.py
+"""
---
+++ embedding/embedding_lookup_sparse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/3/31 下午1:50
+@Author  : YuJun
+@FileName: embedding_lookup_sparse.py
+"""
