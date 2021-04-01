d54748383739c30a93dbe4c6310b4cdd5ab0ea97
549062405@qq.com <yujun001>
2021-04-02 00:17:40 +0800

    commit

---
+++ tensor/eager_execution.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/4/1 下午6:54
+@Author  : YuJun
+@FileName: eager_execution.py
+"""
+
+import tensorflow as tf
+
+print(tf.executing_eagerly())
